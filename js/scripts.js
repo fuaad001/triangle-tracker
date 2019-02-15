90ac62cab109cb81553db0704958e06a1732fc81
fuaad001 <husseinfuaad@hotmail.com>
2019-02-15 14:33:27 +0300

    Link input tags in html with js data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 var track = function(first, second, third){
+  var firstlength = document.getElementById("firstl").value;
+  var secondlength = document.getElementById("secondl").value;
+  var thirdlength = document.getElementById("thirdl").value;
+
+  var first = parseInt(firstlength);
+  var second = parseInt(secondlength);
+  var third = parseInt(thirdlength);
+
    if((first + second > third) && (first + third > second) && (third + second > first)){
     if(first === second && first === third){
       var triangle = "Equilateral";
@@ -11,7 +19,7 @@ var track = function(first, second, third){
     }
   }
    else{
-     var triangle = "Not a triangle";
+     var triangle = "NO triangle at all";
    }
-   alert(triangle);
+   document.getElementById("result").innerHTML = "The type of triangle is: " + triangle;
  }
